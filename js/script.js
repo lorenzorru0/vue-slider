a168bda21110d64306dce7323e1b4e6d218c9377
lorenzorru0 <lorenzoorru999@gmail.com>
2021-09-17 16:30:54 +0200

    Add grafic part

--- js/script.js
+++ js/script.js
@@ -1,9 +1,42 @@
 const vue = new Vue({
     el: "#root",
     data: {
-
+        images: [
+            {
+                url: "img/tuerredda.jpg",
+                name: "Tuerredda"
+            },
+            {
+                url: "img/isArutas.jpg",
+                name: "Is Arutas"
+            },
+            {
+                url: "img/portoOttiolu.jpg",
+                name: "Porto Ottiolu"
+            },
+            {
+                url: "img/calaGoloritze.jpg",
+                name: "Cala Goloritze"
+            },
+            {
+                url: "img/laPelosa.jpg",
+                name: "La Pelosa"
+            },
+        ],
+        imageIndex: 1
     },
     methods: {
-        
+        imageIndexUp: function () {
+            this.imageIndex++;
+            if ( this.imageIndex == this.images.length) {
+                this.imageIndex = 0;
+            }
+        },
+        imageIndexDown: function () {
+            this.imageIndex--;
+            if ( this.imageIndex == -1) {
+                this.imageIndex = this.images.length - 1;
+            }
+        }
     }
 });
